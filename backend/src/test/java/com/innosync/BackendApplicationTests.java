614e2eda694b9ac07ede1e47913733304d3bc3fd
Yusuf Abdughafforzoda <yusufchilling33@gmail.com>
2025-06-11 00:05:03 +0300

    Added simple backend for Hello

---
+++ backend/src/test/java/com/innosync/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.innosync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
