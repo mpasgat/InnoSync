c2248968cb5e5f7a2b3c262e24f35d5ad247ee8f
mpasgat <asgatk5@gmail.com>
2025-06-11 19:05:44 +0300

    first look

---
+++ backend/src/main/resources/static/script.js
@@ -0,0 +1,7 @@
+document.getElementById('managerBtn').addEventListener('click', function() {
+    document.getElementById('result').innerText = 'Result: you are manager';
+});
+
+document.getElementById('participantBtn').addEventListener('click', function() {
+    document.getElementById('result').innerText = 'Result: you are participant';
+});
