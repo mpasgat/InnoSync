3182d3ff994beaa15274e56e4de90d0a0c05e915
Yusuf Abdughafforzoda <yusufchilling33@gmail.com>
2025-06-13 03:03:32 +0300

    feat(auth): added check for length of password

--- backend/src/main/java/com/innosync/controller/AuthController.java
+++ backend/src/main/java/com/innosync/controller/AuthController.java
@@ -13,7 +13,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -30,7 +29,7 @@ public class AuthController {
     @Autowired
     private JwtUtil jwtUtil ;
 
-    private BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+    private final BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 
     @PostMapping("/signup")
     @Operation(summary = "Sign in user")
--- backend/src/main/java/com/innosync/dto/SignInRequest.java
+++ backend/src/main/java/com/innosync/dto/SignInRequest.java
@@ -2,6 +2,7 @@ package com.innosync.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
@@ -10,6 +11,7 @@ public class SignInRequest {
     @Email(message = "Email must be valid")
     String email;
 
+    @Size(min = 8, max = 20, message = "Password's length must be between 8 to 20")
     @NotBlank(message = "Password is required")
     String password;
 }
