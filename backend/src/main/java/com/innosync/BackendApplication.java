b820d64503d5cdab4e121837e62a435e04e90037
Yusuf Abdughafforzoda <yusufchilling33@gmail.com>
2025-06-15 15:49:46 +0300

    feat(auth): added .env file for hiding DB credentials

--- backend/src/main/java/com/innosync/BackendApplication.java
+++ backend/src/main/java/com/innosync/BackendApplication.java
@@ -1,5 +1,6 @@
 package com.innosync;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import org.springframework.boot.SpringApplication;
@@ -10,6 +11,10 @@ import org.springframework.context.annotation.Bean;
 public class BackendApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+		System.setProperty("DB_URL", dotenv.get("DB_URL"));
+		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
 		SpringApplication.run(BackendApplication.class, args);
 	}
 
@@ -21,5 +26,4 @@ public class BackendApplication {
 						.version("1.0")
 						.description("Spring Boot REST API"));
 	}
-
 }
