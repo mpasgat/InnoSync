a85194cf4d0fc553fc8084a4d690ca0e813c5309
Yusuf Abdughafforzoda <yusufchilling33@gmail.com>
2025-06-29 10:03:23 +0300

    feat(project_creation): no update

--- backend/src/main/java/com/innosync/controller/InvitationController.java
+++ backend/src/main/java/com/innosync/controller/InvitationController.java
@@ -7,6 +7,7 @@ import com.innosync.service.InvitationService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
